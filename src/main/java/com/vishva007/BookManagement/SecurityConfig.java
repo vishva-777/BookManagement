fdd85d838555bcf1b7d232deada65c4d378657c0
vishva-777 <vishva6030@gmail.com>
2026-09-11 11:41:32 +0530

    feat: allow deployed S3 frontend as a CORS origin alongside localhost

--- src/main/java/com/vishva007/BookManagement/SecurityConfig.java
+++ src/main/java/com/vishva007/BookManagement/SecurityConfig.java
@@ -82,7 +82,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173","http://vishva-bookmanagement-frontend.s3-website.eu-north-1.amazonaws.com"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
 
